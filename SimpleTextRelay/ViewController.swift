928b1e79f5a028159b45765ce37629309d4403f7
Ronald-Whigan <ronald.whigan95@yahoo.com>
2023-07-02 13:10:30 -0400

    Initial Commit

---
+++ SimpleTextRelay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTextRelay
+//
+//  Created by Ronald Chester Whigan on 7/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
