7bf37b15c46272dc8093bf7c41d07b7d0a4ee6e1
Ronald-Whigan <ronald.whigan95@yahoo.com>
2023-07-28 11:07:47 -0400

    Added Edit Profile that will update Profile View when changes are made

--- SimpleTextRelay/Service/QBService.swift
+++ SimpleTextRelay/Service/QBService.swift
@@ -9,8 +9,12 @@ import Foundation
 import Quickblox
 
 class QBService {
+    
+    ///Shared Instance of QBService
     static var shared = QBService()
     
+    
+    ///Initializes QuickBlox for whole application
     func qbInitialize() {
         let key = QBConstants()
         Quickblox.initWithApplicationId(key.appId, authKey: key.authKey, authSecret: key.authSecret, accountKey: key.acctKey)
@@ -22,6 +26,11 @@ class QBService {
         QBSettings.networkIndicatorManagerEnabled = false
     }
     
+    /// Connects user to Chat server
+    /// - Parameters:
+    ///   - user: Pass in the Current User
+    ///   - completion: Add necessary actions when success
+    ///   - completionError: Add error handling
     func connect(withUser user: QBUUser,connectSuccess completion: @escaping ()->(),_ completionError: @escaping (Error)->()) {
         QBChat.instance.connect(withUserID: user.id, password: user.password!) { error in
             if let error = error {
@@ -32,32 +41,52 @@ class QBService {
         }
     }
     
-    func signIn(whenSuccess completion: @escaping ()->(),_ completionWithSignInError: @escaping (Error)->()) {
+    /// Signs In user / Login User
+    /// - Parameters:
+    ///   - completionHandler: Add QBService.shared.connect here
+    func signIn(with user: String, password: String, completionHandler: @escaping (Result<QBUUser, QBResponse>)->()) {
         //replace temp values later for textfield values
-        QBRequest.logIn(withUserLogin: /*usernameFld.text!*/ "johnDoe", password: /*passwordFld.text!*/ "password", successBlock: { (response, user) in
+        QBRequest.logIn(withUserLogin: user, password: password, successBlock: { (response, user) in
             //Block with response and user instances if the request is succeeded.
             print("Success")
-            QBChat.instance.connect(withUserID: user.id, password: user.password!) { error in
-                completion()
-            }
-            
+            completionHandler(.success(user))
         }, errorBlock: { (response) in
-            completionWithSignInError(response as! Error)
+//            let qbResponse = QBResponseError(response: response)
+//            completionHandler(.failure(qbResponse))
+            completionHandler(.failure(response))
         })
     }
     
+    ///Gets current user
     func getCurrentUser() -> QBUUser {
         guard let curr = QBSession.current.currentUser else {return QBUUser()}
         return curr
     }
-}
-
-protocol SomeProtocol {
-    func thisProtocol()
-}
-
-extension SomeProtocol {
-    func thisProtocol() {
-        //do something
+    
+    ///Signs up user
+    /// - TODO: Fix parameters if needed
+//    func signUp(newUser: QBUUser, completionHandler: @escaping (QBUUser?, String)->()){
+//        QBRequest.signUp(newUser, successBlock: { response, user in
+//            completionHandler(user, "")
+//        }, errorBlock: { (response) in
+//            if let error = response.error?.description {
+//                completionHandler(nil,error)
+//            }
+//        })
+//    }
+    func signUp(newUser: QBUUser, completionHandler: @escaping (Result<QBUUser,Error>)->()){
+        QBRequest.signUp(newUser, successBlock: { response, user in
+            completionHandler(.success(user))
+        }, errorBlock: { (response) in
+            completionHandler(.failure(response))
+        })
     }
+}
+
+struct QBResponseError: Error {
+    let response: QBResponse
+}
+
+extension QBResponse: Error {
+    
 }
